30a620e70cb71af137f9b3e2ca5e74180aad2cf2
Santiago-909 <santiagomezaofficial@gmail.com>
2025-06-09 19:58:06 -0700

    commits

--- game/js/game.js
+++ game/js/game.js
@@ -5,8 +5,8 @@ window.songs = [
 		lyrics:
 			"I I just woke up from a dream where you and I had to say goodbye and I don't know what it all means but since I survived I realized wherever you go that's where I'll follow nobody's promised tomorrow so imma love you every night like it's the last night if the world was ending I'd wanna be next to you if the party was over and our time on Earth was through I'd wanna hold you just for a while and die with a smile if the world was ending I'd wanna be next to you lost lost in the words that was scream I don't even wanna do this anymore cause you already know what you mean to me and our love's the only war worth fighting for wherever you go that's where I'll follow nobody's promised tomorrow if the world was ending I'd wanna be next to you if the party was over and our time on Earth was through I'd wanna hold you just for a while and die with a smile if the world was ending I'd wanna be next to you right next to you next to you right next to you if the world was ending I'd wanna be next to you if the party was over and our time on Earth was through I'd wanna hold you just for a while and die with a smile if the world was ending I'd wanna be next to you if the world was ending Id wanna be next to you I'd wanna be next to you...",
 		file: "../game/videos/dwas.mp4",
-		albumArt:"../game/images/Lady_Gaga_and_Bruno_Mars_-_Die_with_a_Smile.png",
-		startTime:"0.0",
+		albumArt:"images/Lady_Gaga_and_Bruno_Mars_-_Die_with_a_Smile.png",
+		startTime: "",
 	},
 	{
 		id: "feeling",
@@ -221,11 +221,8 @@ window.songs = [
 		title: "CHIHIRO - Billie Eilish",
 		lyrics:
 			"Saw your seat at the counter when I looked away Saw you turn around, but it wasn't your face Said, I need to be alone now, I'm takin' a break How come when I returned, you were gone away? I don't, I don't know why I called I don't know you at all I don't know you Not at all I don't, I don't know why I called I don't know you at all I don't know you Did you take My love away From me? Me",
-		file: "../game/songs/Billie Eilish - CHIHIRO (Audio).mp3",
-		photo: "../game/images/chihiro.jpg",
+		file: "songs/Billie Eilish - CHIHIRO (Audio).mp3",
 		photoSize: "500px",
-		albumArt: "../game/albumart/chihiro.jpg",
-		startTime: "0.0"
 	},
 	{
 		id: "everest",
@@ -234,8 +231,6 @@ window.songs = [
 			"No one's ever known me better Than you Point the arm sleeve of your sweater To the moon You never let—no. Never let me down Everest. I'm ever restless now Now Now We could stay up all night Yeah We could stay up Maybe, for another few light-years Maybe, for life",
 		file: "songs/everest.mp3",
 		photoSize: "500px",
-		albumArt: "..game/images/ab67616d0000b2732bfdb19edb8b2675ba1f2a61.jpg",
-		startTime: "0.0",
 	},
 	{
 		id: "Save a Prayer",
